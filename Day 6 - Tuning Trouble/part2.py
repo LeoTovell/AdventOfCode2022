988830bb454d15fc72742054f9c5f7ae6c2800bb
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2022-12-06 16:14:38 +0000

    Add files via upload

---
+++ Day 6 - Tuning Trouble/part2.py
@@ -0,0 +1,15 @@
+chars = open("input.txt").readlines()[0][:-1]
+
+last4chars = []
+charcount = 0
+for char in chars:
+    last4chars.append(char)
+    charcount += 1
+    if len(last4chars) == 14:
+        if len(set(last4chars)) == 14:
+            print("unique")
+            print(charcount)
+        else:
+            last4chars.pop(0)
+
+# Something fishy here but it works
